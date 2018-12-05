8da2c5d894fa0c6af46101fd0f0d02529831b8d5
Bernardo Calatayud <bernardo.calatayud@mobileappscompany.us>
2018-12-04 23:46:39 -0500

    Initial commit

---
+++ Week7-Daily2/app/src/main/java/com/example/consultants/week7_daily2/customviews/LoginView.java
@@ -0,0 +1,23 @@
+package com.example.consultants.week7_daily2.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class LoginView extends View {
+    public LoginView(Context context) {
+        super(context);
+    }
+
+    public LoginView(Context context, @androidx.annotation.Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoginView(Context context, @androidx.annotation.Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public LoginView(Context context, @androidx.annotation.Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
